5264ef24e8bde19bea89da4177c190c7a9f54a01
Ian Daniher <it.daniher@gmail.com>
2013-09-26 21:53:46 -0400

    improving things slowly...

--- plotData.py
+++ plotData.py
@@ -8,7 +8,6 @@ from matplotlib import pyplot as plt
 b = al.Astral()["Boston"]
 
 # use precise coords from SOS
-print b
 (b.longitude, b.latitude) = (-70.9662135, 42.3177747)
 
 d = pd.read_csv("./209789.csv").replace(to_replace=-9999, value=np.nan)
@@ -19,11 +18,24 @@ d['DATE'] = pd.date_range('1/1/1983', periods=d['DATE'].count())
 
 # calculate total hours daylight as sunset-sunrise
 daylight = map(lambda x: (x['sunset'] - x['sunrise']).total_seconds()/(60*60), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
-elevation = map(lambda x: b.solar_elevation(x['noon']), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
 
-plt.plot(d['DATE'], pd.rolling_mean(d['TMIN'], 30), label="tmin (degC)")
-plt.plot(d['DATE'], pd.rolling_mean(d['TMAX'], 30), label="tmax (degC)")
-plt.plot(d['DATE'], daylight, label="daylight (hrs)")
+
+# http://www.pveducation.org/pvcdrom/properties-of-sunlight/air-mass
+# equation from "Revised optical air mass tables and approximation formula /Fritz Kasten and Andrew T. Young / Applied Optics, Vol. 28, Issue 22, pp. 4735-4738 (1989)"
+# airmass equation incorporates earth curvature
+airmass = lambda ZA: 1.0/(np.cos(ZA*2*np.pi/180.) + (0.50572 * (96.07995 - ZA)**-1.6364))
+# 0.7 -> atmospheric attenuation
+# 0.678 -> emperical fit
+solarFlux = lambda AM: 1.353 * 0.7**AM**0.678
+
+elevation = map(lambda x: b.solar_elevation(x['noon']), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
+print min(elevation), max(elevation)
+
+tmin = list(pd.rolling_mean(d['TMIN'], 30))
+tmax = list(pd.rolling_mean(d['TMAX'], 30))
+
+plt.fill_between(d['DATE'], tmin, tmax, alpha=0.7)
+#plt.plot(d['DATE'], daylight, label="daylight (hrs)")
 plt.plot(d['DATE'], elevation, label="elevation (deg)")
-plt.legend(loc="best")
+#plt.legend(loc="best")
 plt.show()
