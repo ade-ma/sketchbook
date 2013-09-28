020484edf20f8ad9557be7d1aebd90ec9205ccf0
Ian Daniher <it.daniher@gmail.com>
2013-09-28 17:30:01 -0400

    total daily energy estimation completed. need to do weird twinx plotting to make my point

--- plotData.py
+++ plotData.py
@@ -19,26 +19,44 @@ d['DATE'] = pd.date_range('1/1/1983', periods=d['DATE'].count())
 # calculate total hours daylight as sunset-sunrise
 daylight = map(lambda x: (x['sunset'] - x['sunrise']).total_seconds()/(60*60), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
 
-
 # http://www.pveducation.org/pvcdrom/properties-of-sunlight/air-mass
-# equation from "Revised optical air mass tables and approximation formula /Fritz Kasten and Andrew T. Young / Applied Optics, Vol. 28, Issue 22, pp. 4735-4738 (1989)"
+# equation from "Revised optical air mass tables and approximation formula / Fritz Kasten and Andrew T. Young / Applied Optics, Vol. 28, Issue 22, pp. 4735-4738 (1989)"
 # airmass equation incorporates earth curvature
-airmass = lambda ZA: 1.0/(np.cos(np.radians(ZA)) + (0.50572 * (96.07995 - ZA)**-1.6364))
+airMass = lambda ZA: 1.0/(np.cos(np.radians(ZA)) + (0.50572 * (96.07995 - ZA)**-1.6364))
 # 0.7 -> atmospheric attenuation
 # 0.678 -> emperical fit
 # 1.353 -> solar flux constant (kw/m**2)
-solarFlux = lambda AM: 10 * 1.353 * 0.7**(AM**0.678)
+solarFlux = lambda AM: 1353 * 0.7**(AM**0.678)
 
-elevation = map(lambda x: b.solar_elevation(x['noon']), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
+dates = map(lambda x: b.sun(x.to_datetime()), d['DATE'])
 
-earthFlux = map(lambda e: solarFlux(airmass(90-e)), elevation)
+for date in dates[0:365]:
+	intervals = np.array(map(lambda x: x.to_datetime(),
+		pd.date_range(start=date['sunrise'], end=date['sunset'], freq='5Min')))
+	powerDensities = np.array(map(lambda x: solarFlux(airMass(90 - b.solar_elevation(x))),
+		intervals))
+	# multiply power densities by 300 seconds (5 minutes), sum results - forward euler integration
+	plt.plot(intervals[0], sum(np.array([300]*len(intervals)) * powerDensities) , '.k')
 
-plt.plot(d['DATE'], earthFlux)
+plt.ylabel("joules/day")
+plt.title("joules vs. day")
+
+plt.figure()
+
+powerDensityNoon = np.array(map(lambda x:
+	solarFlux(airMass(90-b.solar_elevation(x['noon']))), dates))
+
+plt.plot(d['DATE'][0:365], powerDensityNoon[0:365], label="noon")
+plt.ylabel("power density @ noon")
+
 
 tmin = list(pd.rolling_mean(d['TMIN'], 30))
 tmax = list(pd.rolling_mean(d['TMAX'], 30))
 
-plt.fill_between(d['DATE'], tmin, tmax, alpha=0.7)
-plt.plot(d['DATE'], daylight, label="daylight (hrs)")
-#plt.legend(loc="best")
+plt.figure()
+
+plt.fill_between(d['DATE'][0:365], tmin[0:365], tmax[0:365], alpha=0.7)
+#plt.plot(d['DATE'], daylight, label="daylight (hrs)")
+plt.ylabel("temp (degC)")
+plt.legend(loc="best")
 plt.show()
