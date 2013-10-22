bf88f025178066a3b7276b14fec0009af23bee55
Ian Daniher <it.daniher@gmail.com>
2013-10-22 18:13:08 -0400

    avg yearly temp data

--- plotData.py
+++ plotData.py
@@ -5,6 +5,8 @@ import astral as al
 
 from matplotlib import pyplot as plt
 
+_chunk = lambda l, x: [l[i:i+x] for i in xrange(0, len(l), x)]
+
 b = al.Astral()["Boston"]
 
 # use precise coords from SOS
@@ -15,8 +17,6 @@ d = pd.read_csv("./209789.csv").replace(to_replace=-9999, value=np.nan)
 d['TMAX'] *= 0.1
 d['TMIN'] *= 0.1
 d['DATE'] = pd.date_range('1/1/1983', periods=d['DATE'].count())
-
-yr = d['DATE'][0:365]
 
 # calculate total hours daylight as sunset-sunrise
 daylight = map(lambda x: (x['sunset'] - x['sunrise']).total_seconds()/(60*60), map(lambda x: b.sun(x.to_datetime()), d['DATE']))
@@ -32,36 +32,33 @@ solarFlux = lambda AM: 1353 * 0.7**(AM**0.678)
 
 dates = map(lambda x: b.sun(x.to_datetime()), d['DATE'])
 
-from mpl_toolkits.axes_grid1 import host_subplot
-import mpl_toolkits.axisartist as AA
-
-host = plt.subplot()
-par1 = host.twinx()
-par2 = host.twinx()
-par3 = host.twinx()
-
-par2.plot(yr, daylight[0:365], "k-", label="hours of daylight")
-
 for date in dates[0:365]:
 	intervals = np.array(map(lambda x: x.to_datetime(),
 		pd.date_range(start=date['sunrise'], end=date['sunset'], freq='5Min')))
 	powerDensities = np.array(map(lambda x: solarFlux(airMass(90 - b.solar_elevation(x))),
 		intervals))
 	# multiply power densities by 300 seconds (5 minutes), sum results - forward euler integration
-	host.plot(intervals[0], sum(np.array([300]*len(intervals)) * powerDensities) , '.k')
+	plt.plot(intervals[0], sum(np.array([300]*len(intervals)) * powerDensities) , '.k')
+
+plt.ylabel("joules/day")
+plt.title("joules vs. day")
+
+plt.figure()
 
 powerDensityNoon = np.array(map(lambda x:
 	solarFlux(airMass(90-b.solar_elevation(x['noon']))), dates))
 
-par1.plot(yr, powerDensityNoon[0:365], label="noontime power density")
+plt.plot(d['DATE'][0:365], powerDensityNoon[0:365], label="noon")
+plt.ylabel("power density @ noon")
 
+length = len(_chunk(d['DATE'], 365)[0:-2])
+tmin = sum(np.array(_chunk(d['TMIN'], 365)[0:-2]))/length
+tmax = sum(np.array(_chunk(d['TMAX'], 365)[0:-2]))/length
 
-tmin = list(pd.rolling_mean(d['TMIN'], 2))
-tmax = list(pd.rolling_mean(d['TMAX'], 2))
+plt.figure()
 
-par3.fill_between(yr, tmin[0:365], tmax[0:365], alpha=0.7)
+plt.fill_between(d['DATE'][0:365], tmin[0:365], tmax[0:365], alpha=0.7)
 #plt.plot(d['DATE'], daylight, label="daylight (hrs)")
-par1.legend(loc='lower left')
-par2.legend(loc='lower center')
-par3.legend(loc='lower right')
+plt.ylabel("temp (degC)")
+plt.legend(loc="best")
 plt.show()
